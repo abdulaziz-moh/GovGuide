a59a396772d147e24254dfd427865a0ab587aacf
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-18 18:08:47 +0300

    changed the database to mysql

--- GovGuide/settings.py
+++ GovGuide/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'GovGuide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'govguide_db',
+        'USER': 'root',
+        'PASSWORD':'@1kelimetmiftah',
+        'HOST': 'localhost',
+        'PORT':3306,
+        
     }
 }
 
