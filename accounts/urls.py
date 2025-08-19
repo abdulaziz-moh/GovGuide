52be3986861348197b8f15ca48dddad47ca54f29
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-20 00:17:12 +0300

    created the base template for process app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views, name = 'home')
+    path('signup/',views.signup_veiw, name = 'signup'),
+    path('login/',views.login_view, name = 'login'),
+    path('logout/',views.logout_view, name = 'logout'),
 ]
