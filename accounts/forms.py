f6274b3505d95b101e0fca5383b72afe4b857795
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-20 16:30:51 +0300

    login and signup page completed

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        label=False,
+        widget=forms.EmailInput(attrs={'placeholder':'Email'})
+        )
     
     class Meta:
         model = User
-        fields = ('username','email','password1','password2')
+        fields = ('first_name','last_name','username','email','password1','password2')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        # Programmatically set placeholders for all other fields
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Firstname'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Lastname'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'New Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        
+        # Hide the labels for these fields
+        self.fields['first_name'].label = False
+        self.fields['last_name'].label = False
+        self.fields['username'].label = False
+        self.fields['password1'].label = False
+        self.fields['password2'].label = False
