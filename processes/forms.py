258b0fcb01a6b434d2b5e4e343f244fb41c50fed
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-24 13:08:34 +0300

    adde features: - add data from db when we scroll from the db - creating process functionality , which we can add from 1 to n steps(each step is individual object connected by foreign key) - updated the templates (for their js and some loop html)

--- processes/forms.py
+++ processes/forms.py
@@ -7,8 +7,17 @@ class ProcessForm(forms.ModelForm):
         model = Process
         fields = ('title','description')
 
-class StepFormSet(forms.ModelForm):
+# class StepFormSet(forms.ModelForm):
     
-    class Meta:
-        model = Step
-        fields = ('step_name','description')
+#     class Meta:
+#         model = Step
+#         fields = ('step_name','description')
+        
+from django.forms import inlineformset_factory
+
+StepFormSet = inlineformset_factory(
+    Process, Step,
+    fields=('step_name', 'description'),
+    extra=1,  # just one empty form to start
+    can_delete=True
+)
--- processes/urls.py
+++ processes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('',views, name = 'home')
+    path('create_process/',views.create_process_steps, name = 'create_process'),
+    path('process_list/',views.process_list, name = 'process_list'),
+    path('process_detail/', views.process_detail, name = 'process_detail'),
 ]
