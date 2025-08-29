bbf6cc521a37e83340db8410ddf3e7bf090cfaac
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-29 20:47:34 +0300

    created a working vibrant template for creating a process

--- processes/forms.py
+++ processes/forms.py
@@ -1,5 +1,6 @@
 from . models import Process, Step
 from django import forms
+from django.forms import inlineformset_factory
 
 class ProcessForm(forms.ModelForm):
     
@@ -7,17 +8,22 @@ class ProcessForm(forms.ModelForm):
         model = Process
         fields = ('title','description')
 
-# class StepFormSet(forms.ModelForm):
-    
-#     class Meta:
-#         model = Step
-#         fields = ('step_name','description')
-        
-from django.forms import inlineformset_factory
+class StepForm(forms.ModelForm):
+    class Meta:
+        model = Step
+        fields = ['step_name', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add placeholders
+        self.fields['step_name'].widget.attrs['placeholder'] = 'Step name'
+        self.fields['description'].widget.attrs['placeholder'] = 'Description'
+
 
 StepFormSet = inlineformset_factory(
-    Process, Step,
-    fields=('step_name', 'description'),
-    extra=1,  # just one empty form to start
+    Process,
+    Step,
+    form=StepForm,   # 👈 use custom form here
+    extra=1,
     can_delete=True
 )
