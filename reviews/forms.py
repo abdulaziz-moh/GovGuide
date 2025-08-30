eaecd8efa74ad0d3f7b1fc9b6aa17e602e159452
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-30 20:31:28 +0300

    created model, templates  and it's styles for reveiw

---
+++ reviews/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ["rating", "comment"]
+    def clean_rating(self):
+        rating = self.cleaned_data.get("rating")
+        if rating not in [1, 2, 3, 4, 5]:
+            raise forms.ValidationError("Invalid rating. Must be 1–5 stars.")
+        return rating
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReviewForm
 
 # Create your views here.
+
+def submit_review(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            if request.user.is_authenticated:
+                review.user = request.user
+            review.save()
+            # make status variable and if reviewed give it another veiw like thanks!
+            return redirect("reviews_list")  # redirect after POST
+    else:
+        form = ReviewForm()
+
+    return render(request, "processes/detail.html", {"form": form})
