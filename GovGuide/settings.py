b81e5c4af3edca57cd82db27407fdba76bdd56e9
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-09-05 12:13:59 +0300

    added an email send functionality  for reseting a password

--- GovGuide/settings.py
+++ GovGuide/settings.py
@@ -135,4 +135,20 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # remember to change it to email send
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # remember to change it to email send
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # load variables from .env
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
